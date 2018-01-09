edafe7d3baf2cc019cc2409ece05c2763064839a
jiecong <jiecong@outlook.com>
2018-01-09 16:20:07 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
 import App from './App.vue'
 import CustomCascader from './Cascader.vue'
 import CustomTimePicker from './TimePicker.vue'
 
 Vue.use(ElementUI)
+Vue.use(MintUI)
 
 new Vue({
   el: '#app',
