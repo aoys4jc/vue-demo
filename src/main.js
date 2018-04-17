afb6b1ee146c009d38a10c81039649a15d48d589
aoys4jc <755606093@qq.com>
2018-04-17 09:43:43 +0800

    .

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
+import Vuex from 'vuex';
+import store from '../store/index.js';//导入状态库
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import MintUI from 'mint-ui'
@@ -7,9 +10,84 @@ import App from './App.vue'
 import CustomCascader from './Cascader.vue'
 import CustomTimePicker from './TimePicker.vue'
 import CustomButton from './Button.vue'
+import CustomBottomNav from './BottomNav.vue'
 
 Vue.use(ElementUI)
 Vue.use(MintUI)
+Vue.use(Vuex);
+Vue.use(require('vue-resource'));//引用ajax库
+Vue.use(VueRouter);
+
+
+const routes = [{
+	path: '/',
+	name:"home",
+	component: Ticket,
+	children:[
+		{
+			path:"",
+			name:"ticketbody",
+			component:TicketBody
+		},
+		{
+			path:"/startcity",
+			name:"ticketstartcity",
+			component:TicketStartCity
+		},
+		{
+			path:"/endcity",
+			name:"ticketendcity",
+			component:TicketEndCity
+		},
+		{
+			path:"/result",
+			name:"ticketresult",
+			component:TicketResult
+		},
+		{
+			path: '/ticketpay',
+			name:"ticketpay",
+			component: TicketPay
+		},
+		{
+			path:"*",
+			name:"*ticketbody",
+			component:TicketBody
+		}
+	]
+},
+{
+	path:"*",
+	name:"all",
+	redirect:"/home",
+}
+];
+
+// 3. 创建 router 实例，然后传 `routes` 配置
+// 你还可以传别的配置参数, 不过先这么简单着吧。
+const router = new VueRouter({
+  routes // （缩写）相当于 routes: routes
+});
+
+// router.beforeEach((to, from, next) => {
+// 	if(from.name==="home"){
+// 		store.commit("CHANGE_HEADER",false);
+// 	}
+// 	else{
+// 		store.commit("CHANGE_HEADER",true);
+// 	}
+//   console.log("to",to);
+//   console.log("from",from);
+//   next();
+// })
+
+// 4. 创建和挂载根实例。
+// 记得要通过 router 配置参数注入路由，
+// 从而让整个应用都有路由功能
+const app = new Vue({
+  router,
+  store,
+}).$mount('#app')
 
 new Vue({
   el: '#app',
@@ -17,6 +95,7 @@ new Vue({
     'custom-app':App,
     'custom-cascader':CustomCascader,
     'custom-time':CustomTimePicker,
-    'custom-button':CustomButton
+    'custom-button':CustomButton,
+    'custom-buttom-nav':CustomBottomNav
   }
 })
